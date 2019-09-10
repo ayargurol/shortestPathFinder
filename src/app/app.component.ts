39328679ba010d266256df548d5e0527df09529d
https://gurolayar@bitbucket.org <gurolayar@netaxtech.com>
2019-09-10 19:41:10 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppServiceService } from './services/app-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ShortestPathFinder';
+
+  xValue = 0;
+  yValue = 0;
+  gridVisible: boolean = false;
+  grid = [];
+
+  /**
+   *
+   */
+  constructor(private appService:AppServiceService) {
+    
+  }
+  setUpGrid() {
+    this.grid = [];
+    for (let x = 0; x < this.xValue; x++) {
+      this.grid.push([]);
+      this.grid[x] = [];
+      for (let y = 0; y < this.yValue; y++) {
+        this.grid[x].push({ x, y });
+
+      }
+
+    }
+    this.gridVisible = true;
+  }
+
+  reachNode(){
+    this.appService.publish(`${this.xValue}-${this.yValue}-visit`,{});
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NodeComponent } from './components/node/node.component';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NodeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
